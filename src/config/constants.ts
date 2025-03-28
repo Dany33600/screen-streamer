5e994bf30d18911de2b290670de4c05e499f925a
Dany <dany.bouteiller@gmail.com>
2025-03-28 18:14:44 +0100

    Update constants.ts
    for dev

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,6 +6,6 @@ export const API_PORT = 5070;
 
 // Constante pour forcer le redémarrage de l'onboarding
 // Mettre à true pour forcer le redémarrage de l'onboarding
-export const FORCE_ONBOARDING = false;
+export const FORCE_ONBOARDING = true;
 
 // Autres constantes globales peuvent être ajoutées ici
